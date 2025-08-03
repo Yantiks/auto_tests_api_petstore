7518a089684adc26a76cbb27d2b6d11c44bec408
Yantiks <anaban4na@yandex.ru>
2025-08-03 18:20:24 +0300

    add models and test_pet.py

--- test_data/models.py
+++ test_data/models.py
@@ -1,10 +1,43 @@
 from pydantic import BaseModel
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 class User(BaseModel):
     code: int
     type: str
     message: str
 
-class BaseUser(BaseModel):
-    results: List[User]
+class Pet(BaseModel):
+    id: int
+    category: Category
+    name: Optional[str]
+    photoUrls: List[str]
+    tags: List[Tag]
+    status: str
+
+class Category(BaseModel):
+    id: int
+    name: str
+
+class Tag(BaseModel):
+    id: int
+    name: str
+
+
+# {
+#     "id": 232323,
+#     "category": {
+#         "id": 1,
+#         "name": "cat 1"
+#     },
+#     "name": "capybarinya",
+#     "photoUrls": [
+#         "photourl"
+#     ],
+#     "tags": [
+#         {
+#             "id": 1,
+#             "name": "tagname"
+#         }
+#     ],
+#     "status": "available"
+# }
--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -1,9 +1,18 @@
+from test_data.models import User
 from utils.api.helpers.base_url import HelperBaseUrl
 import requests
+from pydantic import ValidationError
 
 class TestUser:
     def test_user(self, generate_client, set_headers):
         print(generate_client)
         response = requests.request("POST",f'{HelperBaseUrl.BASE_URL_ENV}/user', headers=set_headers, json=generate_client)
-        print(response.status_code)
-        print(response.json())
+
+        user_data = response.json()
+        try:
+            user = User(**user_data) #распаковка словаря в параметры модели
+        except ValidationError as e:
+            print(f"Validation Error: {e}")
+
+        assert user.code == 200, f"Получен код {user.code}, а должен быть 200"
+
